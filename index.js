12601dafdf4731fd215a1ad372bbafbe85057be1
Nikhil Vijayan <nikhil.vijayan@bglgroup.co.uk>
2021-08-14 13:33:14 +0100

    feat: only respond to /random-quote request

--- index.js
+++ index.js
@@ -3,9 +3,14 @@ const quotes = require('./data/quotes.json');
 const PORT = 8080;
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  const random = Math.floor(Math.random() * quotes.length);
-  res.end(`<h1>${quotes[random]}</h1>`);
+  if (req.url === '/random-quote') {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    const random = Math.floor(Math.random() * quotes.length);
+    res.end(`<h1>${quotes[random]}</h1>`);
+  } else {
+    res.writeHead(404, { 'Content-Type': 'text/html' });
+    res.end('<h1>Not Found!</h1><p>Try visiting /random-quote</p>');
+  }
 });
 
 server.listen(PORT, () => {
