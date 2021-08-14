95996dbc6b9041352f80319b2c1afc5033f4ad74
Nikhil Vijayan <nikhil.vijayan@bglgroup.co.uk>
2021-08-14 13:09:23 +0100

    feat: send hello world response

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const http = require('http');
 const PORT = 8080;
 
-const server = http.createServer((req, res) => { })
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/html');
+  res.write('<h1>Hello World</h1>');
+  res.end();
+});
 
 server.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`)
-})
+});
