f5d11556e6c75f8d2bfa7985f10edf1937d1e512
Nikhil Vijayan <nikhil.vijayan@bglgroup.co.uk>
2021-08-14 13:29:53 +0100

    feat: send random quote as h1

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const http = require('http');
+const quotes = require('./data/quotes.json');
 const PORT = 8080;
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html');
-  res.write('<h1>Hello World</h1>');
-  res.end();
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  const random = Math.floor(Math.random() * quotes.length);
+  res.end(`<h1>${quotes[random]}</h1>`);
 });
 
 server.listen(PORT, () => {
-  console.log(`Server listening on ${PORT}`)
+  console.log(`Server listening on ${PORT}`);
 });
